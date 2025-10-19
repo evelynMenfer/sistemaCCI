ca6a066bdceb48ec9c75defc8b15651d1566c1b9
sistemaCCI <mendozaf.evelyn@gmail.com>
2025-10-19 01:04:49 -0600

    ajuste en estadisticas

--- admin/home.php
+++ admin/home.php
@@ -22,10 +22,10 @@
             <span class="info-box-icon bg-info elevation-1"><i class="fas fa-boxes"></i></span>
 
             <div class="info-box-content">
-                <span class="info-box-text">Compras Aceptadas</span>
+                <span class="info-box-text">Finalizadas</span>
                 <span class="info-box-number text-right">
                     <?php
-                    echo $conn->query("SELECT * FROM `receiving_list`")->num_rows;
+                    echo $conn->query("SELECT * FROM `purchase_order_list` WHERE `status` = 3")->num_rows;
                     ?>
                 </span>
             </div>
@@ -40,10 +40,10 @@
             <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-undo"></i></span>
 
             <div class="info-box-content">
-                <span class="info-box-text">Devoluciones</span>
+                <span class="info-box-text">En proceso</span>
                 <span class="info-box-number text-right">
                     <?php
-                    echo $conn->query("SELECT * FROM `return_list`")->num_rows;
+                    echo $conn->query("SELECT * FROM `purchase_order_list` WHERE `status` = 2")->num_rows;
                     ?>
                 </span>
             </div>
@@ -53,13 +53,13 @@
     </div>
     <div class="col-12 col-sm-6 col-md-3">
         <div class="info-box bg-dark shadow">
-            <span class="info-box-icon bg-navy elevation-1"><i class="fas fa-file-invoice-dollar"></i></span>
+            <span class="info-box-icon bg-lightblue elevation-1"><i class="fa fa-check-square"></i></span>
 
             <div class="info-box-content">
-                <span class="info-box-text">Ventas</span>
+                <span class="info-box-text">Autorizado</span>
                 <span class="info-box-number text-right">
                     <?php
-                    echo $conn->query("SELECT * FROM `sales_list`")->num_rows;
+                    echo $conn->query("SELECT * FROM `purchase_order_list` WHERE `status` = 1")->num_rows;
                     ?>
                 </span>
             </div>
@@ -67,6 +67,35 @@
         </div>
         <!-- /.info-box -->
     </div>
+    <div class="col-12 col-sm-6 col-md-3">
+        <div class="info-box bg-dark shadow">
+            <span class="info-box-icon bg-light elevation-1 text-dark">
+                <i class="fa fa-clock"></i>
+            </span>
+            <div class="info-box-content">
+                <span class="info-box-text">Sin autorizar</span>
+                <span class="info-box-number text-right">
+                    <?php echo $conn->query("SELECT * FROM `purchase_order_list` WHERE `status` = 0")->num_rows; ?>
+                </span>
+            </div>
+        </div>
+    </div>
+
+
+    <!--<div class="col-12 col-sm-6 col-md-3">
+        <div class="info-box bg-dark shadow">
+            <span class="info-box-icon bg-navy elevation-1"><i class="fas fa-file-invoice-dollar"></i></span>
+
+            <div class="info-box-content">
+                <span class="info-box-text">Ventas</span>
+                <span class="info-box-number text-right">
+                    <php
+                    echo $conn->query("SELECT * FROM `sales_list`")->num_rows;
+                    ?>
+                </span>
+            </div>
+        </div>
+    </div>-->
     <div class="col-12 col-sm-6 col-md-3">
         <div class="info-box bg-dark shadow">
             <span class="info-box-icon bg-orange elevation-1"><i class="fa fa-building"></i></span>
@@ -76,22 +105,6 @@
                 <span class="info-box-number text-right">
                     <?php
                     echo $conn->query("SELECT * FROM `company_list` where `status` = 1")->num_rows;
-                    ?>
-                </span>
-            </div>
-            <!-- /.info-box-content -->
-        </div>
-        <!-- /.info-box -->
-    </div>
-    <div class="col-12 col-sm-6 col-md-3">
-        <div class="info-box bg-dark shadow">
-            <span class="info-box-icon bg-orange elevation-1"><i class="fas fa-truck-loading"></i></span>
-
-            <div class="info-box-content">
-                <span class="info-box-text">Proveedores</span>
-                <span class="info-box-number text-right">
-                    <?php
-                    echo $conn->query("SELECT * FROM `supplier_list` where `status` = 1")->num_rows;
                     ?>
                 </span>
             </div>
@@ -115,16 +128,33 @@
         </div>
         <!-- /.info-box -->
     </div>
+    <div class="col-12 col-sm-6 col-md-3">
+        <div class="info-box bg-dark shadow">
+            <span class="info-box-icon bg-orange elevation-1"><i class="fas fa-truck-loading"></i></span>
+
+            <div class="info-box-content">
+                <span class="info-box-text">Proveedores</span>
+                <span class="info-box-number text-right">
+                    <?php
+                    echo $conn->query("SELECT * FROM `supplier_list` where `status` = 1")->num_rows;
+                    ?>
+                </span>
+            </div>
+            <!-- /.info-box-content -->
+        </div>
+        <!-- /.info-box -->
+    </div>
+    
     <?php if ($_settings->userdata('type') == 1) : ?>
         <div class="col-12 col-sm-6 col-md-3">
             <div class="info-box bg-dark shadow">
                 <span class="info-box-icon bg-maroon elevation-1"><i class="fas fa-users"></i></span>
 
                 <div class="info-box-content">
-                    <span class="info-box-text">Usuarios</span>
+                    <span class="info-box-text">Clientes</span>
                     <span class="info-box-number text-right">
                         <?php
-                        echo $conn->query("SELECT * FROM `users` where id != 1 ")->num_rows;
+                        echo $conn->query("SELECT * FROM `customer_list` where `status` = 1")->num_rows;
                         ?>
                     </span>
                 </div>
